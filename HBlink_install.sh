31ab85698e8b12747107f0ee8f9ac64d297b7935
LTV <v.konstantin@mail.ru>
2019-07-15 19:41:29 +0100

    Update HBlink_install.sh

--- HBlink_install.sh
+++ HBlink_install.sh
@@ -4,7 +4,6 @@
 
 
 # Globals
-DMR_UTILS="https://github.com/n0mjs710/dmr_utils3.git"
 HBLINK="https://github.com/n0mjs710/HBlink3.git"
 HBMONITOR="https://github.com/n0mjs710/HBmonitor.git"
 PIP="https://bootstrap.pypa.io/get-pip.py"
@@ -13,6 +12,10 @@ DMRUTL_DIR="$WORK_DIR/dmr_utils3"
 HBLINK_DIR="$WORK_DIR/HBlink3"
 HBMON_DIR="$WORK_DIR/HBmonitor"
 HBMON_LOG="/var/log/link"
+SYSCTL="/lib/systemd/system"
+SYSCTL_HBL="hblink.service"
+START_HBM="start_mon.sh"
+SYSCTL_HBM="hbmonitor.service"
 
 ####  Routines  ################################################
 
@@ -44,8 +47,21 @@ installHBlink() {
      Pip3 $HBLINK_DIR/requirements.txt
 }
 
-installHBlinkService() {    
+installHBlinkService() {
+     cd $HBLINK_DIR
+     echo "[Unit]" >> $SYSCTL_HBL
+     echo "Description=Start HBlink" >> $SYSCTL_HBL
+     echo >> $SYSCTL_HBL
+     echo "[Service]" >> $SYSCTL_HBL
+     echo "ExecStart=/usr/bin/python3 $HBLINK_DIR/bridge.py" >> $SYSCTL_HBL
+     echo >> $SYSCTL_HBL
+     echo "[Install]" >> $SYSCTL_HBL
+     echo "WantedBy=multi-user.target" >> $SYSCTL_HBL
+     mv $SYSCTL_HBL $SYSCTL/$SYSCTL_HBL
+     systemctl daemon-reload
+     systemctl enable $SYSCTL_HBL
 }
+
 
 installHBmonitor() {
      echo "Installing HBmonitor"
@@ -58,6 +74,23 @@ installHBmonitor() {
 }
 
 installHBmonitorService() {
+     cd $HBMON_DIR
+     echo "#!/bin/sh" >> $START_HBM
+     echo >> $START_HBM
+     echo "cd $HBMON_DIR" >> $START_HBM
+     echo "/usr/bin/python ./web_tables.py" >> $START_HBM
+     chmod +x $START_HBM
+     echo "[Unit]" >> $SYSCTL_HBM
+     echo "Description=Start HBmonitor" >> $SYSCTL_HBM
+     echo >> $SYSCTL_HBM
+     echo "[Service]" >> $SYSCTL_HBM
+     echo "ExecStart=/usr/bin/python $HBLINK_DIR/$START_HBM" >> $SYSCTL_HBM
+     echo >> $SYSCTL_HBM
+     echo "[Install]" >> $SYSCTL_HBM
+     echo "WantedBy=multi-user.target" >> $SYSCTL_HBM
+     mv $SYSCTL_HBM $SYSCTL/$SYSCTL_HBM
+     systemctl daemon-reload
+     systemctl enable $SYSCTL_HBM
 }
 
 installLibs
